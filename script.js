c1beb1fbb0c901bfadf540c6f34c4e475cad4a4d
Priyansh176 <priyanshsood9@gmail.com>
2024-03-12 17:06:44 +0530

    upload

--- script.js
+++ script.js
@@ -37,18 +37,52 @@ function genLine(){
 let button = document.querySelector("#btn");
 button.addEventListener("click",getVals)
 
+
+
 function getVals(){
     if(forces=='1'){
         var dist1 = document.querySelector("#f1").value
         var force1 = document.querySelector("#F1").value
         var length = document.querySelector("f0").value
+        let load = [[dist1,-force1]]
+        
+        function reactions_sfbdCalc(){
+            //for 1 force
+            let Vb = (-load[0][0]*load[0][1])/length
+            let Va = -load[0][1]-Vb
+            
+            let sf_1 = Va
+            let m_1 = Va*load[0][0]
+            let sf_2 = -Vb
+        
+            let reactions_sfbd = [Va,Vb,sf_1,sf_2,m_1]
+            return reactions_sfbd
+        }
     }
+
     else if(forces=='2'){
         var dist1 = document.querySelector("#f1").value
         var dist2 = document.querySelector("#f2").value
         var force1 = document.querySelector("#F1").value
         var force2 = document.querySelector("#F2").value
+        let load = [[dist1,-force1],[dist2,-force2]]
+        
+        function reactions_sfbdCalc(){
+            // for 2 forces
+            let Vb = ((-load[0][0]*load[0][1])+(-load[1][0]*load[1][1]))/length
+            let Va = -load[0][1]-load[1][1]-Vb
+        
+            let sf_1 = Va
+            let sf_2 = Va + load[0][1]
+            let sf_3 = -Vb
+            let m_1 = Va*load[0][0]
+            let m_2 = Vb*(length-load[1][0])
+        
+            let reactions_sfbd = [Va,Vb,sf_1,sf_2,sf_3,m_1,m_2]
+            return reactions_sfbd
+        }
     }
+
     else if(forces=='3'){
         var dist1 = document.querySelector("#f1").value
         var dist2 = document.querySelector("#f2").value
@@ -56,5 +90,23 @@ function getVals(){
         var force1 = document.querySelector("#F1").value
         var force2 = document.querySelector("#F2").value
         var force3 = document.querySelector("#F3").value
+        let load = [[dist1,-force1],[dist2,-force2],[dist3,-force3]]
+        
+        function reactions_sfbdCalc(){
+            //for 3 forces
+            let Vb = ((-load[0][0]*load[0][1])+(-load[1][0]*load[1][1])+(-load[2][0]*load[2][1]))/length
+            let Va = -load[0][1]-load[1][1]-load[2][1]-Vb
+        
+            let sf_1 = Va
+            let sf_2 = Va + load[0][1]
+            let sf_3 = -load[2][1] - Vb
+            let sf_4 = -Vb
+            let m_1 = Va*load[0][0]
+            let m_2 = Va*load[1][0] + load[0][1]*(load[1][0]-load[0][0])
+            let m_3 = Vb*(length-load[2][0])
+        
+            let reactions_sfbd = [Va,Vb,sf_1,sf_2,sf_3,sf_4,m_1,m_2,m_3]
+            return reactions_sfbd
+        }
     }
 }
